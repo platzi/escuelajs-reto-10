a34e504ccd9fcedb7e5bb871dda35d99bde085d1
Oscar Barajas <oscar@dragons.mx>
2019-11-01 17:41:02 -0500

    The same thing we do every night, Pinky - try to take over the world!

---
+++ src/server/render/index.js
@@ -0,0 +1,26 @@
+const render = (html, preloadedState) => {
+  return `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Platzi Store</title>
+        <link rel="stylesheet" href="assets/app.css" type="text/css"></link>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+        <script>
+        // WARNING: See the following for security issues around embedding JSON in HTML:
+        // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+        window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+    /</g,
+    '\\u003c',
+  )}
+      </script>
+        <script src="assets/app.js" type="text/javascript"></script>
+        <script src="assets/vendor.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `;
+};
+
+export default render;
